817e96d00161c86d2fcbad987403256a18dab4f4
Vitor Duarte <victorduart366@gmail.com>
2019-12-05 02:37:41 -0300

    Implementado popup de cadastro tela de login

---
+++ src/main/webapp/resources/js/cadastro.js
@@ -0,0 +1,8 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    console.log("entrou");
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
