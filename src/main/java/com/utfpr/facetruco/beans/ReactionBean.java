acca5e372d7f14b438d9f54c4c373229113d4123
Vitor Duarte <victorduart366@gmail.com>
2019-12-19 11:19:51 -0300

    the last commit

--- src/main/java/com/utfpr/facetruco/beans/ReactionBean.java
+++ src/main/java/com/utfpr/facetruco/beans/ReactionBean.java
@@ -3,6 +3,9 @@ package com.utfpr.facetruco.beans;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import com.utfpr.facetruco.pojo.Reacao;
+import com.utfpr.facetruco.services.ReacaoService;
+
 
 
 /**
@@ -12,8 +15,15 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean(name = "reactionBean")
 @RequestScoped
 public class ReactionBean {
+
+
    
     public void react(String reacao){
+        Reacao r = new Reacao();
+        r.setPostId(new Long(2));
+        r.setUsername("eduarte");
+        r.setReacao(reacao);
 
+        new ReacaoService().store(r);
     }
 }
--- src/main/java/com/utfpr/facetruco/services/Api.java
+++ src/main/java/com/utfpr/facetruco/services/Api.java
@@ -2,8 +2,8 @@ package com.utfpr.facetruco.services;
 
 
 public class Api{
-    private static final String URIBACKEND = "http://localhost:8080";
-
+    //private static final String URIBACKEND = "http://localhost:8080";
+    private static final String URIBACKEND = "http://192.168.30.105:8080";
     public static String getURIBACKEND(){
         return URIBACKEND;
     }
