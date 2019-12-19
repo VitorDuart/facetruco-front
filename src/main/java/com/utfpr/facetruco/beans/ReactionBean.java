d8e66ea4fc825d82c83e7377a48f9f9e2ffce057
Vitor Duarte <victorduart366@gmail.com>
2019-12-19 09:00:33 -0300

    construindo post

---
+++ src/main/java/com/utfpr/facetruco/beans/ReactionBean.java
@@ -0,0 +1,19 @@
+package com.utfpr.facetruco.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+
+/**
+ * @author vitor
+ */
+
+@ManagedBean(name = "reactionBean")
+@RequestScoped
+public class ReactionBean {
+   
+    public void react(String reacao){
+
+    }
+}
--- src/main/java/com/utfpr/facetruco/beans/UserBean.java
+++ src/main/java/com/utfpr/facetruco/beans/UserBean.java
@@ -1,36 +1,61 @@
-// package com.utfpr.facetruco.beans;
+ /*package com.utfpr.facetruco.beans;
 
-// import java.util.ArrayList;
-// import java.util.List;
 
-// import javax.faces.bean.ManagedBean;
-// import javax.faces.bean.RequestScoped;
-// import javax.inject.Inject;
+ import java.util.List;
 
-// import com.utfpr.facetruco.pojo.User;
+ import javax.faces.bean.ManagedBean;
+ import javax.faces.bean.RequestScoped;
+ import javax.inject.Inject;
 
-// /**
-//  * @author vitor
-//  */
 
-// @ManagedBean(name = "userBean")
-// @RequestScoped
-// public class UserBean {
-//     @Inject
-//     private User user;
-//     private String dia;
-//     private List<String> dias;
+ /**
+ * @author vitor
+ */
+/*
+ @ManagedBean(name = "userBean")
+ @RequestScoped
+ public class UserBean {
+     
+    private String id;
+    @Inject
+    private List<String> dias; 
+    private String dia;
 
-//     public UserBean(){
+
+     public UserBean(){
         
-//     }
+     }
+
+     public String action(String id){
+        return "perfil";
+     }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
 
-//     public User getUser() {return this.user;}
-//     public void setUser(User user) {this.user = user;}
-//     public String getDia() {return this.dia;}
-//     public void setDia(String dia) {this.dia = dia;}
-//     public List<String> getDias() {return this.dias;}
-//     public void setDias(List<String> dias) {this.dias = dias;}
+    public List<String> getDias() {
+        return this.dias;
+    }
 
-// }
+    public void setDias(List<String> dias) {
+        this.dias = dias;
+    }
+
+    public String getDia() {
+        return this.dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+
+}
+
+*/
